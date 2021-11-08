4a891979d9ce69a814cd75c7e9773573b80620ac
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-08 12:55:14 -0500

    Adds dates from moment.js library

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Hello from the JavaScript!");
+
+var date = moment().format("MMMM Do YYYY, h:mm:ss a");
+console.log(date);
+
+var dayEnd = moment().endOf("day").fromNow();
+console.log(dayEnd);
