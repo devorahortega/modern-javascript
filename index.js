a8ffce1b59dc2986ce619d72a0f4db347900ffdd
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-08 13:20:11 -0500

    Changes the text printed in html and javascript

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-console.log("Hello from the JavaScript!");
+console.log("This is the Javascript!");
 
 var date = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(date);
+
+var dayStart = moment().startOf("day").fromNow();
+console.log(dayStart);
 
 var dayEnd = moment().endOf("day").fromNow();
 console.log(dayEnd);
